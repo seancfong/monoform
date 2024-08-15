fe66f1cbd9a413e8a8007c68c86966017bfa3c11
Sean Fong <seancfong@gmail.com>
2024-08-14 20:50:49 -0700

    feat: add basic database query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import { db } from "@/src/db";
+import { usersTable } from "@/src/db/schema";
 
-export default function Home() {
+export default async function Home() {
+  const users = await db.select().from(usersTable);
+
   return (
-    <main className="flex min-h-screen justify-center items-center">
+    <main className="flex min-h-screen flex-col justify-center items-center">
       <div>hello monoform!</div>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </main>
   );
 }
