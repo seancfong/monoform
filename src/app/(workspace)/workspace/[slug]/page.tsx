df902ecad6849be61f57ee6972be18c8884fd80b
Sean Fong <seancfong@gmail.com>
2024-11-25 12:53:42 -0800

    feat: add recent forms

--- src/app/(workspace)/workspace/[slug]/page.tsx
+++ src/app/(workspace)/workspace/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import CreateNew from "@/app/(workspace)/workspace/[slug]/components/dashboard/create-new";
 import FormsGrid from "@/app/(workspace)/workspace/[slug]/components/dashboard/forms-grid";
-import { validateUser } from "@/lib/auth/validate-user";
+import { Suspense } from "react";
 
 type Props = {
   params: { slug: string };
@@ -12,7 +12,9 @@ export default async function DashboardPage({ params }: Props) {
   return (
     <div className="flex w-full flex-col gap-6 px-4 py-4 lg:px-6 lg:py-8">
       <CreateNew slug={slug} />
-      <FormsGrid />
+      <Suspense>
+        <FormsGrid slug={slug} />
+      </Suspense>
     </div>
   );
 }
