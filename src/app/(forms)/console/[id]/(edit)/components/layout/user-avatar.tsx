3d2763bb16cb0a35573adeb0b07728da948372a8
Sean Fong <seancfong@gmail.com>
2024-12-05 17:12:25 -0800

    feat: add user avatar to layout

--- src/app/(forms)/console/[id]/(edit)/components/layout/user-avatar.tsx
+++ src/app/(forms)/console/[id]/(edit)/components/layout/user-avatar.tsx
@@ -1,4 +1,12 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { validateUser } from "@/lib/auth/validate-user";
 import React from "react";
 
@@ -8,8 +16,22 @@ export default async function UserAvatar({}: Props) {
   const { user } = await validateUser();
 
   return (
-    <Avatar className="size-9">
-      <AvatarFallback>{user.email.slice(0, 2).toUpperCase()}</AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-full">
+        <Avatar className="size-9 border-1 border-zinc-200">
+          <AvatarFallback className="font-mono font-medium text-zinc-500">
+            {user.email.slice(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
